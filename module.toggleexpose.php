207db994d60615a5014171a83b3a67d1ea0f363a
Mark Howells-Mead <mark@permanenttourist.ch>
2016-08-20 13:28:32 +0200

    Update module.toggleexpose.php

--- module.toggleexpose.php
+++ module.toggleexpose.php
@@ -1,21 +1,17 @@
 <div class="mod row toggle-expose">
 
 <?php
-
-	function output_images($images, $size){
-
-	    function imagetag($image, $size, $n){
-		    if($n>2){
+	function output_images($images, $size, $initially_visible = 0){
+		function imagetag($image, $size, $n){
+			$prefix = '';
+			$class = '';
+			$button = '';
+			if($n > $initially_visible){
 				$prefix = 'data-';
-				$class = ' class="is-hidden"';
-				$button = '';
-			}elseif($n === 2){
-				$button = '<div class="button-holder"><button class="button button-primary toggle-expose">Expose</button></div>';
-			}else{
-				$prefix = '';
-				$class = '';
-				$button = '';
-			}
+		    	$class = ' class="is-hidden"';
+    		}elseif($n === $initially_visible){
+	    		$button = '<div class="button-holder"><button class="button button-primary toggle-expose">Expose</button></div>';
+		    }
 			return sprintf(
 				'<figure %1$s %2$sstyle="background-image:url(\'%3$s\')"></figure>%4$s',
 				$class,
@@ -24,16 +20,15 @@
 				$button
 			);
 	    }
-	
-	    
+
 	    $n = 0;
-	    
+
 		foreach($images as $image){
 			echo imagetag($image, $size, ++$n);
 		}
 	}
 	
 	$images = array_slice(get_sub_field('images'), 0, intval(get_option('posts_per_page')));
-	output_images($images, 'post-thumbnail-full');
+	output_images($images, 'post-thumbnail-full', 2);
 
 ?></div>
